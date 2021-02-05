4711c4294777dd819ecb48697df312f41c470b99
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-05 17:25:48 +0530

    [vijay]AddedDataEnteringOption

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,6 +7,30 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book");
+            string[] Name = new string[20];
+            string[] Address = new string[50];
+            string[] City = new string[20];
+            string[] State = new string[20];
+            string[] ZipCode = new string[6];
+            string[] PhoneNumber = new string[12];
+            string[] EmailID = new string[20];
+
+            Console.WriteLine("Enter Name");
+            Name[1] = Console.ReadLine();
+            Console.WriteLine("enter Address");
+            Address[1] = Console.ReadLine();
+            Console.WriteLine("enter City");
+            City[1] = Console.ReadLine();
+            Console.WriteLine("enter State");
+            State[1] = Console.ReadLine();
+            Console.WriteLine("enter Zip code");
+            ZipCode[1] = Console.ReadLine();
+            Console.WriteLine("enter Phone number");
+            PhoneNumber[1] = Console.ReadLine();
+            Console.WriteLine("enter Email-Id");
+            EmailID[1] = Console.ReadLine();
+           
+
         }
     }
 }
