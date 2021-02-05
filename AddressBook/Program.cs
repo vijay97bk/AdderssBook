d7f54eab0801ed66f67865adfdc2f78264abb132
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-05 18:22:47 +0530

    [vijay]AbilityToAddNewPerson

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,36 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AddressBook
 {
     class Program
     {
+        public static List<Person> People = new List<Person>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book");
-            string[] Name = new string[20];
-            string[] Address = new string[50];
-            string[] City = new string[20];
-            string[] State = new string[20];
-            string[] ZipCode = new string[6];
-            string[] PhoneNumber = new string[12];
-            string[] EmailID = new string[20];
+            int Choice;
+            
+            Console.WriteLine("1:Add nwe details");
+            Console.WriteLine("2:edit Details");
+            Console.WriteLine("3:Delete details");
+            Choice = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter Name");
-            Name[1] = Console.ReadLine();
-            Console.WriteLine("enter Address");
-            Address[1] = Console.ReadLine();
-            Console.WriteLine("enter City");
-            City[1] = Console.ReadLine();
-            Console.WriteLine("enter State");
-            State[1] = Console.ReadLine();
-            Console.WriteLine("enter Zip code");
-            ZipCode[1] = Console.ReadLine();
-            Console.WriteLine("enter Phone number");
-            PhoneNumber[1] = Console.ReadLine();
-            Console.WriteLine("enter Email-Id");
-            EmailID[1] = Console.ReadLine();
-           
-
+            switch (Choice)
+            {
+                case 1:
+                   AddPerson();
+                    break;
+                case 3:
+                    RemovePerson();
+                    break;
+                case 2:
+                    editPeople();
+                    break;
+             
+                default:
+                    break;
+                   
+            }
         }
+        private static void AddPerson()
+        {
+            Console.Clear();
+
+            Person person = new Person();
+
+            Console.Write("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            person.PhoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Address 1: ");
+            string[] addresses = new string[2];
+            addresses[0] = Console.ReadLine();
+            Console.Write("Enter Address 2 (Optional): ");
+            addresses[1] = Console.ReadLine();
+            person.Addresses = addresses;
+
+            People.Add(person);
+        }
+
     }
 }
